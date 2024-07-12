70bfd145fb0980bba3f778c4c5284aa1fc9b3dbc
Roland Ramadan <129726986+RolandRamadan@users.noreply.github.com>
2024-07-12 09:45:37 +0700

    Initial Commit

---
+++ TestingGitHub/TestingGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGitHubApp.swift
+//  TestingGitHub
+//
+//  Created by Roland Ramadan on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
